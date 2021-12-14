4bb603db5a1f8b4c0bd9c2fa948cd1b673c016dc
Ivan Prokopev <hellsteel@mail.ru>
2021-12-14 23:33:31 +0300

    Initial Commit

---
+++ MVPmyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPmyProject
+//
+//  Created by Ivan Prokopyev on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
